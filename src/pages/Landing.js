ea04951159a71a328671be73d5bb47f2c69e75d3
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-09-13 15:57:13 +0700

    react-router

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,7 @@ import React from 'react'
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/Landing'
 import { Logo } from '../components'
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -12,7 +13,7 @@ const Landing = () => {
         <div className="info">
           <h1>job <span>tracking</span> app</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ducimus voluptas iste dolore repudiandae numquam labore aliquid tenetur id dignissimos?</p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to='/register' className="btn btn-hero">Login/Register</Link>
         </div>
         <img src={main} alt="job hunt" className='img main-img' />
       </div>
