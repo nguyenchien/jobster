d0124fb5ed311a25cfd15335d6d73bdb6c1e6a34
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-09-25 16:37:53 +0700

    shared layout

--- src/pages/dashboard/SharedLayout.js
+++ src/pages/dashboard/SharedLayout.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import {
+  BigSidebar,
+  SmallSidebar,
+  Navbar,
+} from '../../components'
+import Wrapper from '../../assets/wrappers/SharedLayout'
 
 const SharedLayout = () => {
   return (
-    <h1>SharedLayout</h1>
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
   )
 }
 
