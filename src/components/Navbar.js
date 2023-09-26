aae0818ad12cd5b3333d8eaf8bd963abd44f5622
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-09-26 09:43:44 +0700

    react icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/Navbar'
+import { FaHome } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
     <Wrapper>
+      <FaHome />
       <h2>Navbar</h2>
     </Wrapper>
   )
