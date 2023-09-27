8cc47ef8be1a13f65ed36639b975fe54ae831a0e
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-09-27 10:46:19 +0700

    small sidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import Logo from './Logo'
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h2>SmallSidebar</h2>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button className="close-btn" onClick={() => console.log('toggle')}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            nav links
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
