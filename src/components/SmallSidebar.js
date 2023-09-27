fd8431aef158ed9ddd40c1abf480d0c5aa563240
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-09-27 14:16:19 +0700

    small sidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -2,13 +2,20 @@ import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import Wrapper from '../assets/wrappers/SmallSidebar'
 import Logo from './Logo'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleSidebar } from '../features/user/userSlice'
 
 const SmallSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  }
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div className={isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
         <div className="content">
-          <button className="close-btn" onClick={() => console.log('toggle')}>
+          <button className="close-btn" onClick = { toggle }>
             <FaTimes />
           </button>
           <header>
