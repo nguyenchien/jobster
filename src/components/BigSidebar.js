8873e2b272ffd0ad8be18dd085a243505b7725f9
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-09-28 13:19:08 +0700

    big sidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,10 +1,20 @@
-import React from 'react'
 import Wrapper from '../assets/wrappers/BigSidebar'
+import Logo from './Logo'
+import NavLinks from './NavLinks'
+import { useSelector } from 'react-redux'
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
     <Wrapper>
-      <h2>BigSidebar</h2>
+      <div className={isSidebarOpen ? 'sidebar-container' : 'sidebar-container show-sidebar'}>
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   )
 }
