b3a28c8bd502160cc4e28439015d246e189f4c36
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-10-31 10:18:49 +0700

    JobsContainer setup

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useEffect } from 'react'
+import Job from './Job'
+import Wrapper from '../assets/wrappers/JobsContainer'
+import { useSelector, useDispatch } from 'react-redux'
+import Loading from './Loading'
 
 const JobsContainer = () => {
+  const {jobs, isLoading} = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+  
+  if (isLoading) {
+    return (
+      <Loading center />
+    )
+  }
+  
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No Jobs to display...</h2>
+      </Wrapper>
+    )
+  }
+  
   return (
-    <h2>JobsContainer</h2>
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div className="jobs">
+        {jobs.map((job, index) => {
+          return <Job key={index} {...job} />
+        })}
+      </div>
+    </Wrapper>
   )
 }
 
--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import jobSlice from './features/job/jobSlice';
 import userSlice from './features/user/userSlice';
-import alljobsSlice from './features/alljobs/alljobsSlice';
+import allJobsSlice from './features/allJobs/allJobsSlice';
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     job: jobSlice,
-    alljobs: alljobsSlice,
+    allJobs: allJobsSlice,
   }
 })
