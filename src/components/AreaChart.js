08b5b87208c6eecaa245340ac3adde13df3b49e4
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-02 15:47:04 +0700

    charts container

---
+++ src/components/AreaChart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AreaChart = ( {data} ) => {
+  console.log(data);
+  return (
+    <h2>AreaChart</h2>
+  )
+}
+
+export default AreaChart
---
+++ src/components/BarChart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BarChart = ( {data} ) => {
+  console.log(data);
+  return (
+    <h2>BarChart</h2>
+  )
+}
+
+export default BarChart
