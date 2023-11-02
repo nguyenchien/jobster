8e7ed875ffdd3d514724fca69de843e047d609c1
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-02 16:12:12 +0700

    charts container

--- src/components/StatItem.js
+++ src/components/StatItem.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/StatItem'
 
-const StatItem = () => {
+const StatItem = ({title, count, icon, color, bcg}) => {
   return (
-    <h2>StatItem</h2>
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className="count">{count}</span>
+        <span className="icon">{icon}</span>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
   )
 }
 
