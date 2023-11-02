c629b3d8e7abd5c29ed0024fd3ce090f54dbceff
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-02 10:01:14 +0700

    status page container

---
+++ src/components/StatsContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StatsContainer = () => {
+  return (
+    <h2>StatsContainer</h2>
+  )
+}
+
+export default StatsContainer
--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import {Loading, StatsContainer, ChartsContainer} from '../../components'
 import { showStats } from '../../features/allJobs/allJobsSlice'
 
 const Stats = () => {
   const dispath = useDispatch();
+  
+  const {
+    isLoading,
+    stats,
+    monthlyApplications,
+  } = useSelector((store) => store.allJobs);
   
   useEffect(() => {
     dispath(showStats());
   }, []);
   
   return (
-    <h1>Status</h1>
+    <>
+      <StatsContainer/>
+      {monthlyApplications.length > 0 && <ChartsContainer/>}
+    </>
   )
 }
 
