d4bbcfe8da57152d3f3b335beea6b4c8475b2ed9
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-03 09:45:15 +0700

    charts complete

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
 
-const BarChart = ( {data} ) => {
-  console.log(data);
+const BarChartComponent = ( {data} ) => {
   return (
-    <h2>BarChart</h2>
+    <ResponsiveContainer width='100%' height={300}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 50,
+        }}
+      >
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='date' />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey='count' fill='#2cb1bc' barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
   )
 }
 
-export default BarChart
+export default BarChartComponent
