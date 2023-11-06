9b67ede4c67d932bb0819485a30208cab2121220
nguyen.van.chien <nguyen.van.chien@m-hand.com>
2023-11-06 10:59:29 +0700

    checkForUnauthorizedResponse function

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getUserFromLocalStorage } from "./localStorage";
+import { logoutUser } from "../features/user/userSlice";
 
 const customFetch = axios.create({
   baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
@@ -14,5 +15,13 @@ customFetch.interceptors.request.use((config) => {
 }, (error) => {
   return Promise.reject(error);
 });
+
+export const checkForUnauthorizedResponse = (error, thunkAPI) => {
+  if (error.response.statusCode === 401) {
+    thunkAPI.dispatch(logoutUser());
+    return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+  }
+  return thunkAPI.rejectWithValue(error.response.data.msg);
+}
 
 export default customFetch;
